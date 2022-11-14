4daa567dd6114ec12517fde90d18d82052e5fbb1
h-sara <115357536+h-sara@users.noreply.github.com>
2022-11-14 10:28:13 +0000

    [Finish] 5-10

--- db/migrate/20221113081413_create_post_images.rb
+++ db/migrate/20221113081413_create_post_images.rb
@@ -2,7 +2,7 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
       #投稿画像を識別するID
-      t.integer :id
+      #t.integer :id
       # お店の名前
       t.string :shop_name
       # 画像の名前
